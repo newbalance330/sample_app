bb358644225c446abefb480bdf8595d16e5c99fe
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-11-04 14:51:28 +0000

    [finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2faa9c6bedad36232405b1a0924ce60181df2a209aec79c297cfe96039f71ea67932d79b4cd9ce65d8c5be55155b2b883c944a4673d47651cc3a6bac63ddc30b'
